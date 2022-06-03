b341d2a874e3d2fc613eb6163161919ede0a3450
Ernesto Méndez <mendezcode@gmail.com>
2022-06-03 13:13:30 -0400

    Update package name on package.json

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '76658831c91b5bd68a86');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'a447816faf3786ac9dc9');
